db8a43fb46808674b9d02fcf703833d0bebbbaea
matkatmusic <matkatmusic@gmail.com>
2020-01-14 19:29:35 +0000

    Added PART1 instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
-#include <iostream>
+/*
+ Project 4 - Part 1 / 9
+ video: Chapter 2 Part 7
+ Returning Values tasks 
 
+ Create a branch named Part1
+ 
+ 1) write 3 UDTs named FloatType, DoubleType, IntType.
+ 
+ 2) give each UDT the following member functions:
+        add( lhs, rhs );
+        subtract( lhs, rhs );
+        multiply( lhs, rhs );
+        divide( lhs, rhs );
+ 
+ 3) make them return the correct primitive type. e.g. if you're implementing the FloatType::add function, your implementation would start like this:
+        float FloatType::add( float lhs, float rhs )
+ 
+ 4) declare a few instances of each class in main() and call the member functions with the appropriate arguments.
+        FloatType ft;
+        auto result = ft.add(3.2f, 23.f );
+ 
+ 5) print out those results using std::cout:
+        std::cout "result of ft.add(): " << result << std::endl;
+ 
+ 6) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ Send me the the link to your repl.it in a DM on Slack
+
+ Wait for my code review.
+ */
+
+#include <iostream>
 int main()
 {
     std::cout << "good to go!" << std::endl;
-    return 0;    
 }
