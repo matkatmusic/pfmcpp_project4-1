95333570dc9c0fdc1682e8a421ecf26e941050ef
matkatmusic <matkatmusic@gmail.com>
2020-01-15 02:32:12 +0000

    Added PART2 Instructions. Note line 5

--- main.cpp
+++ main.cpp
@@ -1,29 +1,56 @@
 /*
- Project 4 - Part 1 / 9
- video: Chapter 2 Part 7
- Returning Values tasks 
+ Project 4 - Part 2 / 9
+ Video: Chapter 3 Part 6
 
- Create a branch named Part1
+ ******* Create a branch named Part2 ********
+
+New/This/Pointers/References conclusion
+
+    Build/Run often with this task to make sure you're not breaking the code with each step.
+    I recommend committing after you get each step working so you can revert to a working version easily if needed.
  
- 1) write 3 UDTs named FloatType, DoubleType, IntType.
+ 0) in the blank space below, declare/define an empty struct named 'A' on a single Line. 
+     on the lines below it, write a struct named 'HeapA' that correctly shows how to own an instance of 'A' 
+         on the heap without leaking, without using smart pointers. 
+ */
+
+
+
+
+
+
+
+
+
+
+
+ /*
+ 1) Edit your 3 structs so that they own a heap-allocated primitive type without using smart pointers  
+         IntType should own a heap-allocated int, for example.
  
- 2) give each UDT the following member functions:
-        add( lhs, rhs );
-        subtract( lhs, rhs );
-        multiply( lhs, rhs );
-        divide( lhs, rhs );
+ 2) give it a constructor that takes the appropriate primitive
+    this argument will initialize the owned primitive's value.
+         i.e. if you're owning an int on the heap, your ctor argument will initialize that heap-allocated int's value.
  
- 3) make them return the correct primitive type. e.g. if you're implementing the FloatType::add function, your implementation would start like this:
-        float FloatType::add( float lhs, float rhs )
+ 3) modify those add/subtract/divide/multiply member functions from chapter 2 on it
+         a) make them modify the owned numeric type
+         b) set them up so they can be chained together.
+             i.e.
+             DoubleType dt(3.5);
+             dt.add(3.0).multiply(-2.5).divide(7.2); //an example of chaining
  
- 4) declare a few instances of each class in main() and call the member functions with the appropriate arguments.
-        FloatType ft;
-        auto result = ft.add(3.2f, 23.f );
+ 4) write some add/subtract/divide/multiply member functions for each type that take your 3 UDTs
+         for example, IntType::divide(const DoubleType& dt);
  
- 5) print out those results using std::cout:
-        std::cout "result of ft.add(): " << result << std::endl;
+ 5) print out the results with some creative couts 
+    i.e.
+         FloatType ft(0.1f);
+         IntType it(3);
+         std::cout << "adding 3 and subtracting 'it' from 'ft' results in the following value: " << *ft.add(2.f).subtract( it ).value << std::endl;  //note the dereference of the `value` member of `ft`
  
- 6) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ 6) Don't let your heap-allocated owned type leak!
+ 
+ 7) click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
@@ -31,6 +58,9 @@
 
  Wait for my code review.
  */
+
+
+
 
 #include <stdexcept>
 #include <iostream>
